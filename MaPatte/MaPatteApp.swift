877e2682e08e4421769d6bb32d33984923f8b846
manonjarzabeck <manonjarzabeck@gmail.com>
2026-06-03 15:41:37 +0200

    Initial Commit

---
+++ MaPatte/MaPatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaPatteApp.swift
+//  MaPatte
+//
+//  Created by Manon  on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaPatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
