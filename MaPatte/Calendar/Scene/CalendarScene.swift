ea6127d1e9a9d6a763cecf1074d00f76541f5cdd
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2026-06-04 19:42:54 +0200

    feat(Calendar): add empty CalendarScene

---
+++ MaPatte/Calendar/Scene/CalendarScene.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarScene.swift
+//  MaPatte
+//  
+//  Created by Mathieu Nivelles on 04/06/2026.
+//  Copyright © 2026 Mathieu Nivelles. All rights reserved.
+//  
+
+import SwiftUI
+
+struct CalendarScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarScene()
+}
