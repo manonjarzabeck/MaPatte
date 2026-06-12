f9b156580864e08ae6cde060a6ba2937cd3cfb54
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2026-06-12 21:49:05 +0200

    feat(App): use AppTabBar in ContentView

--- MaPatte/ContentView.swift
+++ MaPatte/ContentView.swift
@@ -9,25 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            Tab("Fiche", systemImage: "info.circle") {
-                MedicalSheetScene()
-            }
-            
-            Tab("Calendrier", systemImage: "calendar") {
-                NavigationStack {
-                    CalendarScene()
-                }
-            }
-            
-            Tab("Documents", systemImage: "doc") {
-                DocumentsScene()
-            }
-            
-            Tab("Vétérinaires", systemImage: "stethoscope") {
-                VeterinarianRecordsScene()
-            }
-        }
+        AppTabBar()
     }
 }
 
